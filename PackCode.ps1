469ec196d9eb4c18b232286af7ae59a022bee463
binbinku <2822230340@qq.com>
2026-01-30 18:44:14 +0800

    opt out

--- PackCode.ps1
+++ PackCode.ps1
@@ -1,17 +1,26 @@
-$json = Get-Content "./PackCodeCfg.json" -Raw | ConvertFrom-Json
-$fileList_h = $json.fileList_h;
-$fileList_cpp = $json.fileList_cpp;
-
-
-function PackFile($hfile)
+﻿function InitOutFolder($path)
 {
-    Write-Host "Pack to ["$hfile.name"]" 
+    if (Test-Path $path) 
+    {
+        Remove-Item "$path\*" -Recurse -Force
+    } else 
+    {
+        New-Item -ItemType Directory -Path $path | Out-Null
+    }
+}
 
-    $outPath = "./PackOut/$($hfile.name)";
+
+function PackFile($hfile, $outFolder)
+{
+    Write-Host "Pack to ["$hfile.name"]" -ForegroundColor Yellow
+
+    $outPath = "$outFolder/$($hfile.name)";
+
     "" | Out-File $outPath -Encoding utf8
+    
     foreach ($file in $hfile.files) 
     {
-        Write-Host "Packing:" $file
+        # Write-Host "Packing:" $file
 
         $filename = Split-Path $file -Leaf
         "<BKFile name=$($filename)>`n`n" | Add-Content $outPath -Encoding utf8
@@ -24,9 +33,55 @@ function PackFile($hfile)
 }
 
 
-Write-Host "==========[Files]=========="
 
-foreach ($hfile in $fileList_h ) 
+# [Main Entry]
+
+
+Clear-Host
+
+Write-Host "`n`n`n`n"
+
+Write-Host @"
+   ██████╗  ██████╗ ██████╗ ███████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗
+  ██╔════╝ ██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+  ██║      ██║   ██║██║  ██║█████╗  ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+  ██║      ██║   ██║██║  ██║██╔══╝  ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+  ╚██████╗ ╚██████╔╝██████╔╝███████╗██║     ██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+   ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+"@ -ForegroundColor Green
+
+
+$files = Get-ChildItem .\Configs\*.json
+
+Write-Host "`n============[Config-List-Start]===========`n" -ForegroundColor Blue
+foreach ($file in $files) 
 {
-    PackFile($hfile)
+    $json = Get-Content $file.FullName -Raw | ConvertFrom-Json
+    Write-Host "[Config]: $($file.name) Export: $($json.isExport)"
+}
+Write-Host "`n============[Config-List-End]===========`n" -ForegroundColor Blue
+
+
+foreach ($file in $files) 
+{
+    $json = Get-Content $file.FullName -Raw | ConvertFrom-Json
+
+    if($json.isExport -eq $false)
+    {
+        continue;
+    }
+
+    $fileName = $file.name -replace '.json', ''
+    $outFolder = "./PackOut/$fileName";
+
+    InitOutFolder($outFolder);
+
+    Write-Host "`n`n==========[Export-Start-$fileName]==========`n" -ForegroundColor Green
+
+    foreach ($hfile in $json.packList) 
+    {
+        PackFile $hfile $outFolder
+    }
+    
+    Write-Host "`n==========[Export-End-$fileName]==========`n`n" -ForegroundColor Green
 }
